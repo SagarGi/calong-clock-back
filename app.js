4b8d9849fc1e29d48e8a9d9df9d34b618c82aa85
sagar <sagargurung1001@gmail.com>
2025-10-07 08:23:24 +0100

    Set up mysql database connection
    Signed-off-by: sagar <sagargurung1001@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +12,28 @@ app.use(express.json());
 // Test route
 app.get("/", (req, res) => {
   res.send("Server is Running to Accept API Requests");
+});
+
+// testing database connection
+async function testDatabaseConnection() {
+  try {
+    const result = await db.query("SELECT NOW() AS currentTime");
+    console.log("✅ Database connected successfully!");
+    console.log("Current Time from DB:", result[0].currentTime);
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+  }
+}
+
+app.get("/database", async (req, res) => {
+  try {
+    const result = await db.query("SELECT NOW() AS currentTime");
+    res.send(
+      "Database is also running, Current time: " + result[0].currentTime
+    );
+  } catch (error) {
+    res.status(500).json({ error: "Database is not connected!" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
